e345e6813a36557f16eb6a96e58fd14139859ba0
bohdans <0000xffff@gmail.com>
2019-02-27 18:04:33 +0200

    JL - 0001: Setup project - group files - add constants file - add Pod framework

---
+++ UATuber/Sources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  UATuber
+//
+//  Created by Bogdan Sasko on 2/27/19.
+//  Copyright © 2019 vinso. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "AIzaSyABBMILJA7tXHJRt6cQqDoDiZXTuU-POrA"
+}
--- UATuberUITests/UATuberUITests.swift
+++ UATuberUITests/UATuberUITests.swift
@@ -30,5 +30,4 @@ class UATuberUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
 }
